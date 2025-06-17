3f8d0758733e47bb3fe04f8d2926793b6f23fc17
Bowerick Ultrajax <>
2025-06-17 02:26:04 +0200

    Reformatted files and added buffer as import

--- src/exporter.js
+++ src/exporter.js
@@ -1,5 +1,6 @@
-import sha1 from 'sha1';
-import Zip from 'jszip';
+import sha1 from "sha1";
+import Zip from "jszip";
+import Buffer from "buffer";
 
 export default class {
   constructor(deckName, { template, sql }) {
@@ -15,22 +16,26 @@ export default class {
     this.media = [];
     this.topDeckId = topDeckId;
     this.topModelId = topModelId;
-    this.separator = '\u001F';
+    this.separator = "\u001F";
 
-    const decks = this._getInitialRowValue('col', 'decks');
+    const decks = this._getInitialRowValue("col", "decks");
     const deck = getLastItem(decks);
     deck.name = this.deckName;
     deck.id = topDeckId;
-    decks[topDeckId + ''] = deck;
-    this._update('update col set decks=:decks where id=1', { ':decks': JSON.stringify(decks) });
+    decks[topDeckId + ""] = deck;
+    this._update("update col set decks=:decks where id=1", {
+      ":decks": JSON.stringify(decks),
+    });
 
-    const models = this._getInitialRowValue('col', 'models');
+    const models = this._getInitialRowValue("col", "models");
     const model = getLastItem(models);
     model.name = this.deckName;
     model.did = this.topDeckId;
     model.id = topModelId;
     models[`${topModelId}`] = model;
-    this._update('update col set models=:models where id=1', { ':models': JSON.stringify(models) });
+    this._update("update col set models=:models where id=1", {
+      ":models": JSON.stringify(models),
+    });
   }
 
   save(options) {
@@ -41,21 +46,21 @@ export default class {
       return prev;
     }, {});
 
-    zip.file('collection.anki2', Buffer.from(binaryArray));
-    zip.file('media', JSON.stringify(mediaObj));
+    zip.file("collection.anki2", Buffer.from(binaryArray));
+    zip.file("media", JSON.stringify(mediaObj));
 
     media.forEach((item, i) => zip.file(i, item.data));
 
-    if (process.env.APP_ENV === 'browser' || typeof window !== 'undefined') {
-      return zip.generateAsync(Object.assign({}, { type: 'blob' }, options));
+    if (process.env.APP_ENV === "browser" || typeof window !== "undefined") {
+      return zip.generateAsync(Object.assign({}, { type: "blob" }, options));
     } else {
       return zip.generateAsync(
         Object.assign(
           {},
           {
-            type: 'nodebuffer',
+            type: "nodebuffer",
             base64: false,
-            compression: 'DEFLATE'
+            compression: "DEFLATE",
           },
           options
         )
@@ -73,48 +78,51 @@ export default class {
     const note_guid = this._getNoteGuid(topDeckId, front);
     const note_id = this._getNoteId(note_guid, now);
 
-    let strTags = '';
-    if (typeof tags === 'string') {
+    let strTags = "";
+    if (typeof tags === "string") {
       strTags = tags;
     } else if (Array.isArray(tags)) {
       strTags = this._tagsToStr(tags);
     }
 
-    this._update('insert or replace into notes values(:id,:guid,:mid,:mod,:usn,:tags,:flds,:sfld,:csum,:flags,:data)', {
-      ':id': note_id, // integer primary key,
-      ':guid': note_guid, // text not null,
-      ':mid': topModelId, // integer not null,
-      ':mod': this._getId('notes', 'mod', now), // integer not null,
-      ':usn': -1, // integer not null,
-      ':tags': strTags, // text not null,
-      ':flds': front + separator + back, // text not null,
-      ':sfld': front, // integer not null,
-      ':csum': this._checksum(front + separator + back), //integer not null,
-      ':flags': 0, // integer not null,
-      ':data': '' // text not null,
-    });
+    this._update(
+      "insert or replace into notes values(:id,:guid,:mid,:mod,:usn,:tags,:flds,:sfld,:csum,:flags,:data)",
+      {
+        ":id": note_id, // integer primary key,
+        ":guid": note_guid, // text not null,
+        ":mid": topModelId, // integer not null,
+        ":mod": this._getId("notes", "mod", now), // integer not null,
+        ":usn": -1, // integer not null,
+        ":tags": strTags, // text not null,
+        ":flds": front + separator + back, // text not null,
+        ":sfld": front, // integer not null,
+        ":csum": this._checksum(front + separator + back), //integer not null,
+        ":flags": 0, // integer not null,
+        ":data": "", // text not null,
+      }
+    );
 
     return this._update(
-      'insert or replace into cards values(:id,:nid,:did,:ord,:mod,:usn,:type,:queue,:due,:ivl,:factor,:reps,:lapses,:left,:odue,:odid,:flags,:data)',
+      "insert or replace into cards values(:id,:nid,:did,:ord,:mod,:usn,:type,:queue,:due,:ivl,:factor,:reps,:lapses,:left,:odue,:odid,:flags,:data)",
       {
-        ':id': this._getCardId(note_id, now), // integer primary key,
-        ':nid': note_id, // integer not null,
-        ':did': topDeckId, // integer not null,
-        ':ord': 0, // integer not null,
-        ':mod': this._getId('cards', 'mod', now), // integer not null,
-        ':usn': -1, // integer not null,
-        ':type': 0, // integer not null,
-        ':queue': 0, // integer not null,
-        ':due': 179, // integer not null,
-        ':ivl': 0, // integer not null,
-        ':factor': 0, // integer not null,
-        ':reps': 0, // integer not null,
-        ':lapses': 0, // integer not null,
-        ':left': 0, // integer not null,
-        ':odue': 0, // integer not null,
-        ':odid': 0, // integer not null,
-        ':flags': 0, // integer not null,
-        ':data': '' // text not null
+        ":id": this._getCardId(note_id, now), // integer primary key,
+        ":nid": note_id, // integer not null,
+        ":did": topDeckId, // integer not null,
+        ":ord": 0, // integer not null,
+        ":mod": this._getId("cards", "mod", now), // integer not null,
+        ":usn": -1, // integer not null,
+        ":type": 0, // integer not null,
+        ":queue": 0, // integer not null,
+        ":due": 179, // integer not null,
+        ":ivl": 0, // integer not null,
+        ":factor": 0, // integer not null,
+        ":reps": 0, // integer not null,
+        ":lapses": 0, // integer not null,
+        ":left": 0, // integer not null,
+        ":odue": 0, // integer not null,
+        ":odid": 0, // integer not null,
+        ":flags": 0, // integer not null,
+        ":data": "", // text not null
       }
     );
   }
@@ -123,7 +131,7 @@ export default class {
     this.db.prepare(query).getAsObject(obj);
   }
 
-  _getInitialRowValue(table, column = 'id') {
+  _getInitialRowValue(table, column = "id") {
     const query = `select ${column} from ${table}`;
     return this._getFirstVal(query);
   }
@@ -137,12 +145,12 @@ export default class {
   }
 
   _tagsToStr(tags = []) {
-    return ' ' + tags.map(tag => tag.replace(/ /g, '_')).join(' ') + ' ';
+    return " " + tags.map((tag) => tag.replace(/ /g, "_")).join(" ") + " ";
   }
 
   _getId(table, col, ts) {
     const query = `SELECT ${col} from ${table} WHERE ${col} >= :ts ORDER BY ${col} DESC LIMIT 1`;
-    const rowObj = this.db.prepare(query).getAsObject({ ':ts': ts });
+    const rowObj = this.db.prepare(query).getAsObject({ ":ts": ts });
 
     return rowObj[col] ? +rowObj[col] + 1 : ts;
   }
@@ -154,9 +162,9 @@ export default class {
 
   _getNoteId(guid, ts) {
     const query = `SELECT id from notes WHERE guid = :guid ORDER BY id DESC LIMIT 1`;
-    const rowObj = this.db.prepare(query).getAsObject({ ':guid': guid });
+    const rowObj = this.db.prepare(query).getAsObject({ ":guid": guid });
 
-    return rowObj.id || this._getId('notes', 'id', ts);
+    return rowObj.id || this._getId("notes", "id", ts);
   }
 
   _getNoteGuid(topDeckId, front) {
@@ -165,13 +173,13 @@ export default class {
 
   _getCardId(note_id, ts) {
     const query = `SELECT id from cards WHERE nid = :note_id ORDER BY id DESC LIMIT 1`;
-    const rowObj = this.db.prepare(query).getAsObject({ ':note_id': note_id });
+    const rowObj = this.db.prepare(query).getAsObject({ ":note_id": note_id });
 
-    return rowObj.id || this._getId('cards', 'id', ts);
+    return rowObj.id || this._getId("cards", "id", ts);
   }
 }
 
-export const getLastItem = obj => {
+export const getLastItem = (obj) => {
   const keys = Object.keys(obj);
   const lastKey = keys[keys.length - 1];
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import Exporter from './exporter.js';
-import createTemplate from './template.js';
-import sql from 'sql.js/js/sql-memory-growth.js';
+import Exporter from "./exporter.js";
+import createTemplate from "./template.js";
+import sql from "sql.js/js/sql-memory-growth.js";
 
 export { Exporter };
 
-export default function(deckName, template) {
+export default function (deckName, template) {
   return new Exporter(deckName, {
     template: createTemplate(template),
-    sql
+    sql,
   });
 }
